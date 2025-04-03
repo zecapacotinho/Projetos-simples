1fd3003ba81893cad123b24f33c312eefd5782ea
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-03 19:48:34 -0300

    atz

---
+++ Anáslisador Númerico/js/script.js
@@ -0,0 +1,23 @@
+// Variaveis
+const res = window.document.querySelector('#resul')
+const select = window.document.querySelector('#select')
+
+// Funções
+function ClickButton(){
+    let num = window.document.querySelector('#num').value
+    if(!num.trim()){
+        window.alert('Por favor, coloque um valor!')
+    }
+    else{
+        
+    }
+}
+
+function ClickRes(){
+    
+}
+
+
+
+
+
