3b3a28c7d5ddd7cb0722d9f66c91bf7b1e28d5f9
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-04-06 17:23:41 -0300

    atz

--- Relógio Digital/js/script.js
+++ Relógio Digital/js/script.js
@@ -1,21 +1,28 @@
 //Função para o tema escuro/claro
-const check = document.getElementById('check')
+const check = window.document.getElementById('check')
 check.addEventListener('change', () => {
     document.body.classList.toggle('dark')
-    
 })
-
-//Função para mudar o tipo de hora
 
 //Função para o relógio
 function Horario(){
     const horario = window.document.querySelector('.hora')
     const agora = new Date()
-    const hora = agora.getHours().toString().padStart(2, '0')
+    let hora = agora.getHours()
     const minutos = agora.getMinutes().toString().padStart(2, '0')
     const segundos = agora.getSeconds().toString().padStart(2, '0')
+    const chk = window.document.getElementById('chk')
+    if(chk.checked){
+        hora %= 12
+        if(hora === 0){
+            hora = 12
+        }
+    }
+    else{
+        hora %= 24
+    }
+    hora = hora.toString().padStart(2, '0')
     horario.innerHTML = `${hora}:${minutos}:${segundos}`
-    
 }
 Horario()
 setInterval(Horario, 1000)
